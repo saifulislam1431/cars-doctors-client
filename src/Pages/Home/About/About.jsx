c41dc878874168b2c130379fa62a8a53e34f70d0
Saiful Islam <sakibbhaiya143@gmail.com>
2023-05-10 23:13:24 +0600

    About section added

--- src/Pages/Home/About/About.jsx
+++ src/Pages/Home/About/About.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import parts from "../../../assets/images/about_us/parts.jpg"
+import person from "../../../assets/images/about_us/person.jpg"
 
 const About = () => {
     return (
-        <div>
-            
-        </div>
+        <section>
+            <div className="hero min-h-screen">
+                <div className="hero-content flex-col lg:flex-row items-center justify-center">
+                    <div className='w-full relative mr-7 mb-20 lg:mb-0'>
+                    <img src={person} className="rounded-lg shadow-2xl w-3/5" />
+                    <img src={parts} className=" rounded-lg shadow-2xl w-1/2 absolute top-1/2 left-1/3 border-8 border-white" />
+                    </div>
+                    <div>
+                        <p className='text-primary font-bold text-xl mb-3'>About Us</p>
+                        <h1 className="text-5xl font-bold">We are qualified & of experience in this field</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <p className="py-2">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button className="mySecBtn mt-5">Get More Info</button>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import About from '../About/About';
 
 
 const Home = () => {
     return (
         <section className='my-10 md:mt-12 lg:mt-16 space-y-10'>
             <Banner />
+            <About />
 
         </section>
     );
