c9a55a63a82f129484b73e9a7794e3ab968d0567
Saiful Islam <sakibbhaiya143@gmail.com>
2023-05-10 19:38:42 +0600

    Router setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
+   content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#db2733",
+        
+"secondary": "#757ef9",
+        
+"accent": "#db46bf",
+        
+"neutral": "#1F1D34",
+        
+"base-100": "#F0F0F5",
+        
+"info": "#388ADC",
+        
+"success": "#1A6B5B",
+        
+"warning": "#FBDE60",
+        
+"error": "#E24658",
+        },
+      },
+    ],
   },
-  plugins: [],
+   plugins: [require("daisyui")],
 }
 
