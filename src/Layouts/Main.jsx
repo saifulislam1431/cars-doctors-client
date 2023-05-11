4c678b52be1053f626f79d980a45c8ce2688449f
Saiful Islam <sakibbhaiya143@gmail.com>
2023-05-11 22:00:40 +0600

    Register authentication added added

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Pages/Shared/Footer/Footer';
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const Main = () => {
     return (
         <section>
             <Navbar />
             <Outlet />
             <Footer />
+            <ToastContainer />
         </section>
     );
 };
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,15 +1,141 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import login from "../../assets/images/login/login.svg";
 import icon1 from "../../assets/icons/facebook.png";
 import icon2 from "../../assets/icons/linkedin.png";
 import icon3 from "../../assets/icons/google.png";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserContext } from '../../Provider/Auth/Auth';
+import { toast } from 'react-toastify';
+import { updateProfile } from 'firebase/auth';
 
 const Register = () => {
-    const handleRegister = e =>{
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+    const from = location.state?.from?.pathname || '/';
+    const { signUp,
+        googleIn,
+        facebookIn } = useContext(UserContext);
+
+
+    const handleRegister = e => {
         e.preventDefault();
-        
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, email, password);
+
+        if (password.length < 6) {
+            return setError("Your password should be minimum six in length")
+        }
+
+        signUp(email, password)
+            .then(res => {
+                const loggedUser = res.user;
+                navigate(from, { replace: true });
+                toast.success('Registration Successful!', {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                });
+                setError("")
+                form.reset();
+                updateProfile(loggedUser, {
+                    displayName: name
+                }).then(() => console.log("Update")).catch((error) => console.log(error.message))
+                console.log(loggedUser);
+            })
+            .catch((error) => {
+                toast.error(error.message, {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                });
+            })
+
     }
+
+    const handleGoogleIn= ()=>{
+        googleIn()
+        .then(res => {
+            const loggedUser = res.user;
+            navigate(from, { replace: true });
+            toast.success('Registration Successful!', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
+            setError("")
+
+            updateProfile(loggedUser, {
+                displayName: name
+            }).then(() => console.log("Update")).catch((error) => console.log(error.message))
+
+        })
+        .catch((error) => {
+            toast.error(error.message, {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
+        })
+    }    
+
+    const handleFacebookIn= ()=>{
+        facebookIn()
+        .then(res => {
+            const loggedUser = res.user;
+            navigate(from, { replace: true });
+            toast.success('Login Successful!', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
+            setError("")
+
+            updateProfile(loggedUser, {
+                displayName: name
+            }).then(() => console.log("Update")).catch((error) => console.log(error.message))
+
+        })
+        .catch((error) => {
+            toast.error(error.message, {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
+        })
+    } 
     return (
         <section>
             <div className="hero min-h-screen">
@@ -21,12 +147,12 @@ const Register = () => {
                         <h1 className='text-center pt-5 text-2xl font-bold'>Register</h1>
                         <div className="card-body">
                             <form onSubmit={handleRegister}>
-                            <div className="form-control">
+                                <div className="form-control">
                                     <label className="label">
                                         <span className="label-text">Name</span>
                                     </label>
                                     <input type="text" placeholder="name"
-                                    name='name' className="input input-bordered" />
+                                        name='name' className="input input-bordered" />
                                 </div>
 
                                 <div className="form-control">
@@ -34,7 +160,7 @@ const Register = () => {
                                         <span className="label-text">Email</span>
                                     </label>
                                     <input type="text" placeholder="email"
-                                    name='email' className="input input-bordered" />
+                                        name='email' className="input input-bordered" />
                                 </div>
 
 
@@ -42,26 +168,26 @@ const Register = () => {
                                     <label className="label">
                                         <span className="label-text">Password</span>
                                     </label>
-                                    <input type="password" placeholder="password" 
-                                name='password'
-                                    className="input input-bordered" />
+                                    <input type="password" placeholder="password"
+                                        name='password'
+                                        className="input input-bordered" />
 
                                 </div>
                                 <input type="submit" value="Sign Up" className="btn btn-primary form-control mt-6 w-full" />
 
                             </form>
-                            <hr className='border-1 border-primary mt-2'/>
+                            <hr className='border-1 border-primary mt-2' />
                             <div className='text-center items-center mt-3'>
                                 <p className='font-medium'>or Sign in with</p>
                                 <div className='my-3 space-x-3'>
-                                    <button>
-                                        <img src={icon1} alt=""className='w-8' />
+                                    <button onClick={handleFacebookIn}>
+                                        <img src={icon1} alt="" className='w-8' />
                                     </button>
                                     <button>
-                                        <img src={icon2} alt=""className='w-8' />
+                                        <img src={icon2} alt="" className='w-8' />
                                     </button>
-                                    <button>
-                                        <img src={icon3} alt=""className='w-8' />
+                                    <button onClick={handleGoogleIn}>
+                                        <img src={icon3} alt="" className='w-8' />
                                     </button>
                                 </div>
                                 <p>Have an account? <Link to="/login" className='text-primary font-semibold'>Sign In</Link></p>
